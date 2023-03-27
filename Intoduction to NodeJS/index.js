472c2b0622601bb35d8f6fbe1e3b054c28bf436a
Uche2828 <105387128+Uche2828@users.noreply.github.com>
2023-03-27 12:36:54 +0100

    Add files via upload

---
+++ Intoduction to NodeJS/index.js
@@ -0,0 +1,7 @@
+
+const fullName = 'Uche Ugwari';
+const preferredLanguage = 'NodeJS';
+const myFuture = 'I see myself in the next one year, being a well skilled Backend Developer and working in one of the top tech companies in the world.'
+
+const info= `My name is ${fullName} and my preferred programming language is ${preferredLanguage}. ${myFuture} `;
+console.log(info);
