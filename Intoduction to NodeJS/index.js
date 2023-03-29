32abdd35d517f19a1baa68fdfd19c5a705acc1f5
Uche2828 <105387128+Uche2828@users.noreply.github.com>
2023-03-29 12:31:40 +0100

    Update index.js

--- Intoduction to NodeJS/index.js
+++ Intoduction to NodeJS/index.js
@@ -1,7 +1,3 @@
 
-const fullName = 'Uche Ugwari';
-const preferredLanguage = 'NodeJS';
-const myFuture = 'I see myself in the next one year, being a well skilled Backend Developer and working in one of the top tech companies in the world.'
 
-const info= `My name is ${fullName} and my preferred programming language is ${preferredLanguage}. ${myFuture} `;
-console.log(info);
+console.log('My name is Uchechi Ugwari and my preferred programming language is NodeJs. I see myself in the next one year, being a well skilled Backend Developer and working in one of the top tech companies in the world.')
